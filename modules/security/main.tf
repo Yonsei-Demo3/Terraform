5bed965230869a367d762eacd9b5f92fa271dad2
leerura <sw09112@naver.com>
2025-11-09 17:11:41 +0900

    FEAT: rds 보안그룹 추가

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -34,3 +34,36 @@ resource "aws_security_group" "api_sg" {
     Name = "${var.project_name}-api-sg"
   }
 }
+
+resource "aws_security_group" "rds_sg" {
+  name        = "${var.project_name}-rds-sg"
+  description = "Security group for RDS"
+  vpc_id      = var.vpc_id
+
+  # 인바운드 규칙: API 서버에서 오는 MySQL 트래픽 허용
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.api_sg.id]
+  }
+
+  ingress {
+    from_port = 3306
+    to_port   = 3306
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # 아웃바운드 규칙: 모든 트래픽 허용
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-rds-sg"
+  }
+}
